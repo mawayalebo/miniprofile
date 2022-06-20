b7174f206f4547a193a43f25efed5979b90df5b0
NTE ADMIN <mawayalebo@gmail.com>
2022-06-20 07:48:03 +0200

    injecting skills from stackshare

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,15 @@
+import Head from "next/head";
+
 function Skills() {
     return ( 
-        <div>
+        <div className="w-full h-full">
+            <Head>
+                <script async src="https://cdn1.stackshare.io/javascripts/client-code.js"></script>
+            </Head>
             <div>
                 <h2>black belting skills</h2>
             </div>
-            <a data-theme="light" data-layers="1,2,3,4" data-stack-embed="true" href="https://embed.stackshare.io/stacks/embed/e33559230764f11b9b49413c5893bc"></a><script async src="https://cdn1.stackshare.io/javascripts/client-code.js" ></script>
+            <a data-theme="light" data-layers="2,1,3,4" data-stack-embed="true" href="https://embed.stackshare.io/stacks/embed/e33559230764f11b9b49413c5893bc"></a>
         </div>
      );
 }
