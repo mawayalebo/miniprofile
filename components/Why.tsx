6b6c4c111688f7c711a7ff9228ba6e1f82c32a4e
NTE ADMIN <mawayalebo@gmail.com>
2022-06-20 09:14:44 +0200

    why sovtech

---
+++ components/Why.tsx
@@ -0,0 +1,18 @@
+function Why() {
+    return ( 
+        <section>
+            {/* why want to join a company? */}
+            <div>
+                <h1></h1>
+            </div>
+            <div>
+                I have been longing to join a company that can help me to grow my skills and knowledge.
+                The graduate programme is a gateway to get a job in the industry.
+                Sovtech uses the technology I enjoy working with, for example Javascript.
+                My main goal is to better our country using the same technology
+            </div>
+        </section>
+     );
+}
+
+export default Why;
